eedc1808bcd933caa8fce801d4a722c2be8eeccf
SoumyaS4 <110401215+SoumyaS4@users.noreply.github.com>
2023-05-10 15:39:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,11 @@ chrome_driver_path = "C:\selenium\chromedriver.exe"
 service_obj= Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome()
 
-# driver.get("https://www.amazon.com/Ovente-KG83B-BPA-Free-Protection-Indicator/dp/B00DEPGY7G/ref=sr_1_2_sspa?crid=1Q8HNX37C5T7P&keywords=kettle&qid=1683468363&sprefix=kett%2Caps%2C350&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFZRUdQTTZWRFNKVFEmZW5jcnlwdGVkSWQ9QTA3NTE3MTQyR0Y2Tzg0TzdVSFlDJmVuY3J5cHRlZEFkSWQ9QTA1MTY5MjMxR0JQODRENkNaNzVFJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==")
-# price = driver.find_elements(By.CLASS_NAME,"a-offscreen")
-# lists=[]
-#     prices = ele.get_attribute("innerHTML")
-#     lists.append(prices)
-# print(lists[0])
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, "cookie")
 
-# I think timeout should be set bigger to inscrease the possibility for buying more expensive tool
+# I think timeout should be set bigger to increase the possibility for buying more expensive tool
 timeout = time.time() + 10
 five_min = time.time() + 60 * 5
 
